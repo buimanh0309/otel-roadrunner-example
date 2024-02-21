03cde93b656592f017b342229cf51a7ad007ddfe
Finn <huan.nv@tinasoft.vn>
2024-02-21 21:17:58 +0700

    serviceB example

---
+++ service/serviceB/app/Http/Controllers/ValueController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ValueController extends Controller
+{
+    public function getValue()
+    {
+        $client = new Client();
+
+        try {
+            $response = $client->request('GET', 'http://127.0.0.1:8001/api/getvalues');
+
+            $data = $response->getBody()->getContents();
+
+            $result = json_decode($data, true);
+
+            return response()->json($result);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+}
